614981ae42472f12ee2b2c275d3a56c58d556276
Cheesy Cheese <66090357+AdanTheCheese@users.noreply.github.com>
2025-11-26 23:44:12 +0100

    Add files via upload

--- js1.js
+++ js1.js
@@ -10,30 +10,47 @@ class Otazka {
 
 seznam_otazek = {
     otazka0:["Čáp", "obrazek", 0, ["Option 1\n", "Option 2\n", "Option 3", "Option 4"], 0],
-    otazka1:["Bobr", "obrazek", 0, ["Option 1\n", "Option 2\n", "Option 3", "Option 4"], 1]
-}
+    otazka1:["Bobr", "obrazek", 0, ["Option 1\n", "Option 2\n", "Option 3", "Option 4"], 1]}
 
-otazka = "" 
+otazka = ""
 
 function hrat() {
     function vyhodnot(tip) {
         if (tip == otazka.spravna_odpoved) {
             text.innerText = "To je správná odpověď";
-            document.body.appendChild(dalsi_button);
         } else {
             text.innerText = "To je špatně!!!!";
         }
     }
 
     function dalsi() {
-        doc
+        random_otazka = Math.floor(Math.random() * Object.keys(seznam_otazek).length);
+
+        otazka = new Otazka(seznam_otazek["otazka" + random_otazka][0], seznam_otazek["otazka" + random_otazka][1], seznam_otazek["otazka" + random_otazka][2], seznam_otazek["otazka" + random_otazka][3], seznam_otazek["otazka" + random_otazka][4]);
+        delete seznam_otazek["otazka" + random_otazka];
+
+        document.getElementById("otazka_element").innerText = otazka.jmeno_otazky;
+        for (let i = 0; i < 4; i++) {
+            document.getElementById("otazka_button").remove();
+        }
+
+        for (let i = 0; i < otazka.otazky.length; i++) {
+            const para = document.createElement("button");
+            para.innerText = otazka.otazky[i];
+            para.id = "otazka_button"
+            para.addEventListener("click", () => vyhodnot(i));
+            document.body.appendChild(para);
+
+            document.body.appendChild(document.createElement("br"));
+        }
     }
 
     function zobrazeni_otazky() {
-        random_otazka = Math.floor(Math.random() * 2);
+        random_otazka = Math.floor(Math.random() * Object.keys(seznam_otazek).length);
 
         otazka = new Otazka(seznam_otazek["otazka" + random_otazka][0], seznam_otazek["otazka" + random_otazka][1], seznam_otazek["otazka" + random_otazka][2], seznam_otazek["otazka" + random_otazka][3], seznam_otazek["otazka" + random_otazka][4]);
-    
+        delete seznam_otazek["otazka" + random_otazka];
+
         document.getElementById("hrat_button").style.display = "none";
         document.getElementById("otazka_element").innerText = otazka.jmeno_otazky;
 
